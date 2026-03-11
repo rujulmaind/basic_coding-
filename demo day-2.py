9bbb07537f0967b232535ecd572fd8c211e60593
Rujul Maind <rujulmaind74@gmail.com>
2026-03-11 11:48:29 +0530

    update

--- demo day-2.py
+++ demo day-2.py
@@ -131,18 +131,18 @@ mylist=["apple","banana","cherry","orange","kiwi","melon","mango","grapes","pine
 #     print("Sorry! You are not eligible for placement.")
 
 #write a program to accept 5 different value in 5 different variable and check maximum value and print by using simple if statement
-num1=int(input("Enter number 1: ")) 
-num2=int(input("Enter number 2: "))
-num3=int(input("Enter number 3: "))
-num4=int(input("Enter number 4: "))
-num5=int(input("Enter number 5: "))
-if num1>num2 and num1>num3 and num1>num4 and num1>num5:
-    print("Maximum number is: ",num1)
-if num2>num1 and num2>num3 and num2>num4 and num2>num5:
-    print("Maximum number is: ",num2)
-if num3>num1 and num3>num2 and num3>num4 and num3>num5:
-    print("Maximum number is: ",num3)
-if num4>num1 and num4>num2 and num4>num3 and num4>num5:
-    print("Maximum number is: ",num4)   
-if num5>num1 and num5>num2 and num5>num3 and num5>num4:
-    print("Maximum number is: ",num5)
+# num1=int(input("Enter number 1: ")) 
+# num2=int(input("Enter number 2: "))
+# num3=int(input("Enter number 3: "))
+# num4=int(input("Enter number 4: "))
+# num5=int(input("Enter number 5: "))
+# if num1>num2 and num1>num3 and num1>num4 and num1>num5:
+#     print("Maximum number is: ",num1)
+# if num2>num1 and num2>num3 and num2>num4 and num2>num5:
+#     print("Maximum number is: ",num2)
+# if num3>num1 and num3>num2 and num3>num4 and num3>num5:
+#     print("Maximum number is: ",num3)
+# if num4>num1 and num4>num2 and num4>num3 and num4>num5:
+#     print("Maximum number is: ",num4)   
+# if num5>num1 and num5>num2 and num5>num3 and num5>num4:
+#     print("Maximum number is: ",num5)
