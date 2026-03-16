c3e89f32f1aef9b5d715a099dc07e570895943ad
Rujul Maind <rujulmaind74@gmail.com>
2026-03-16 10:26:34 +0530

    update

--- Demo day-5.py
+++ Demo day-5.py
@@ -2,8 +2,15 @@ def msg(): # called function
     print("Hello World")
     n1 = int(input("Enter the value of n1: "))
     n2 = int(input("Enter the value of n2: "))
-    print("Add:", n1+n2)
+    sum = n1 + n2
+    sub = n1 - n2
+    mul = n1 * n2
+    div = n1 / n2
+    return sum, sub, mul, div
+result = msg()
+print(result)
 
 
 
-msg() # calling function
+
+
