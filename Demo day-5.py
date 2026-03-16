93f6ff5eba62918104777d3b4c71dd57ecd9fda1
Rujul Maind <rujulmaind74@gmail.com>
2026-03-16 10:20:05 +0530

    Initial commit

---
+++ Demo day-5.py
@@ -0,0 +1,9 @@
+def msg(): # called function
+    print("Hello World")
+    n1 = int(input("Enter the value of n1: "))
+    n2 = int(input("Enter the value of n2: "))
+    print("Add:", n1+n2)
+
+
+
+msg() # calling function
