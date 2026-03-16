f55fcbb3e6b02a1d335fc48e39676b58705944b0
Rujul Maind <rujulmaind74@gmail.com>
2026-03-16 10:37:35 +0530

    update

--- Demo day-5.py
+++ Demo day-5.py
@@ -1,16 +1,36 @@
-def msg(): # called function
-    print("Hello World")
-    n1 = int(input("Enter the value of n1: "))
-    n2 = int(input("Enter the value of n2: "))
-    sum = n1 + n2
-    sub = n1 - n2
-    mul = n1 * n2
-    div = n1 / n2
-    return sum, sub, mul, div
-result = msg()
-print(result)
+# def msg(): # called function
+#     print("Hello World")
+#     n1 = int(input("Enter the value of n1: "))
+#     n2 = int(input("Enter the value of n2: "))
+#     sum = n1 + n2
+#     sub = n1 - n2
+#     mul = n1 * n2
+#     div = n1 / n2
+#     return sum, sub, mul, div
+# result = msg()
+# print(result)
 
 
+# positional arguments
+# keyword arguments
+# default arguments
+# variable length arguments/ variable no. of arguments
+
+# 1. positional arguments
+# def personalInfo(fname, lname):
+#     print("First name: ", fname)
+#     print("Last name: ", lname)
+
+# fname="Rujul"
+# lname="Maind"
+# personalInfo(fname, lname)
 
 
+# 2. Default argument
+def cityName(city="Nagpur"):
+     print("City: ", city)
 
+
+cityName("New York")
+cityName("Mumbai")
+cityName()
