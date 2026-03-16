2b15d37c8b582d65b1f5b065f489fff7dbf2d631
Rujul Maind <rujulmaind74@gmail.com>
2026-03-16 11:34:16 +0530

    update

--- Demo day-5.py
+++ Demo day-5.py
@@ -45,6 +45,10 @@ mylist=[1,2,3,4,5]
 def searchElement(target):
     for i in range(len(mylist)):
         if target==mylist[i]:
-             print("Element found at index: ", i)
-             break
-searchElement(3)
+             return i
+    return -1
+result = searchElement(9)
+if result != -1:
+    print("Element found at index: ", result)
+else:
+    print("Element not found in the list")
